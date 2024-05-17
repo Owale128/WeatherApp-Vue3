49cb4c632124814873d5c64fe17220d8aed4978b
Owale128 <Christian.buaka@hotmail.com>
2024-05-17 19:21:23 +0200

    Added a fetchWeather function to fetch the api call from getWeather

--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { getWeather } from '../services/apiCall'
+import { WeatherData } from '../models/WeatherModel.ts';
 
+const city = ref('');
+const weather = ref<WeatherData | null>(null);
+const apiKey = import.meta.env.VITE_APP_API_KEY;
+
+const fetchWeather = async () => {
+  try {
+    if (city.value) {
+      weather.value = await getWeather(city.value, apiKey);
+      console.log(weather.value);
+    }
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
+};
+
+onMounted(() => {
+  fetchWeather();
+});
 </script>
 
 <template>
-  <div>
 
-  </div>
 </template>
 
 <style lang="scss" scoped>
